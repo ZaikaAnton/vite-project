2df90413640e9fe89c0b79a42b2821db400a226b
Anton Zaika <lycan44444@gmail.com>
2024-06-13 22:40:13 +0200

    add card with product

--- src/Pages/Menu/Menu.tsx
+++ src/Pages/Menu/Menu.tsx
@@ -1,6 +1,7 @@
 import styles from "./Menu.module.css";
 import Headling from "../../components/Headling/Headling";
 import Search from "../../components/Search/Search";
+import ProductCard from "../../components/ProductCard/ProductCard";
 
 export function Menu() {
   return (
@@ -8,6 +9,16 @@ export function Menu() {
       <div className={styles.head}>
         <Headling>Меню</Headling>
         <Search placeholder="Введите блюдо или состав" />
+      </div>
+      <div>
+        <ProductCard
+          id={1}
+          title="Наслаждение"
+          description="Салями, руккола, помидоры, оливки"
+          rating={4.5}
+          price={300}
+          image="/product.png"
+        />
       </div>
     </>
   );
